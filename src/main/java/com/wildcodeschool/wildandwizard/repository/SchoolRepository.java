c3ee5fb97857144c04530c570b2624717c182507
Stuchlikom <stuchlikom@gmail.com>
2020-01-20 11:37:05 +0100

    JPA for school table implemented

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,9 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.wildcodeschool.wildandwizard.entity.School;
 
-public interface SchoolRepository {
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
 }
